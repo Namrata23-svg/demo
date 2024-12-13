cb098022980e3b04499f7c4e9d18bc7e3db86971
Namrata <nm890234@gmail.com>
2024-12-13 16:03:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React,{Component} from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './Components/Home'
+
+class App extends React.Component {
+ 
+render(){
+  return (
+    <>
+    <Home/>
+    </>
+  )
+}}
+
+export default App
---
+++ src/Components/Home.jsx
@@ -0,0 +1,40 @@
+import React,{Component} from "react";
+import { fetchData } from "../../../Controllers/api";
+
+class Home extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+        data:null,
+        error:null,
+    }
+  }
+ async componentDidMount(){
+  
+    try{
+  const data=await fetchData()
+  this.setState({data})
+    }
+    catch(error)
+    {
+  this.setState({error})
+    }
+}
+
+    render(){
+const{data,error}=this.state
+
+        return(
+<div>
+<h1>data</h1>
+{data && data.map((item, index) => (
+            <li key={index}>{item.name}</li> 
+          ))}
+          <button>previous</button>
+          <button>Next</button>
+</div>
+        )
+    }
+} 
+
+export default Home;
