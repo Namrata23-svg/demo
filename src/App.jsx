2cdc28d3860ccc04f6ff31da6c711b3050ef5348
Namrata <nm890234@gmail.com>
2024-12-18 10:21:16 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './Components/Home'
+
+import Item from '../../Controllers/ItemList'
 
 class App extends React.Component {
  
 render(){
   return (
     <>
-    <Home/>
+    <Item/>
     </>
   )
 }}
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,40 +1,41 @@
-import React,{Component} from "react";
-import { fetchData } from "../../../Controllers/api";
+// import React,{Component} from "react";
+// import { fetchData } from "../../../Controllers/ItemList";
 
-class Home extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={
-        data:null,
-        error:null,
-    }
-  }
- async componentDidMount(){
+// class Home extends React.Component{
+//   constructor(props){
+//     super(props);
+//     this.state={
+//         data:null,
+//         error:null,
+
+//     }
+//   }
+//  async componentDidMount(){
   
-    try{
-  const data=await fetchData()
-  this.setState({data})
-    }
-    catch(error)
-    {
-  this.setState({error})
-    }
-}
+//     try{
+//   const data=await fetchData()
+//   this.setState({data})
+//     }
+//     catch(error)
+//     {
+//   this.setState({error})
+//     }
+// }
 
-    render(){
-const{data,error}=this.state
+//     render(){
+// const{data,error}=this.state
 
-        return(
-<div>
-<h1>data</h1>
-{data && data.map((item, index) => (
-            <li key={index}>{item.name}</li> 
-          ))}
-          <button>previous</button>
-          <button>Next</button>
-</div>
-        )
-    }
-} 
+//         return(
+// <div>
+// <h1>data</h1>
+// {data && data.map((item, index) => (
+//             <li key={index}>{item.name}</li> 
+//           ))}
+//           <button>previous</button>
+//           <button>Next</button>
+// </div>
+//         )
+//     }
+// } 
 
-export default Home;
+// export default Home;
